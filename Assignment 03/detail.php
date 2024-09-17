59328957615bef2e6bdaf3b557c4b4fe1d6a6aae
Anna-Dinius <156371391+Anna-Dinius@users.noreply.github.com>
2024-09-17 12:53:25 -0400

    added bootstrap styles & css

--- Assignment 03/detail.php
+++ Assignment 03/detail.php
@@ -11,15 +11,29 @@ $post = getPost($posts, $index);
 
 <head>
   <title><?= $post['title'] ?></title>
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="blog.css" rel="stylesheet">
 </head>
 
 <body>
-  <h1><?= $post['title'] ?></h1>
-  <p><strong>Author</strong>:</p> <?= $post['author'] ?></p>
-  <p><strong>Date:</strong></p> <?= $post['date'] ?></p>
-  <p><?= $post['content'] ?></p>
+  <header class="lh-1 py-3 mb-4">
+    <a class="text-decoration-none" href="index.php">
+      <h1 class="container blog-header-logo">Blog</h1>
+    </a>
+  </header>
+
+  <main class="container">
+    <header>
+      <h3 class="display-6 mb-1"><?= $post['title'] ?></h3 class="display-5 link-body-emphasis mb-1">
+    </header>
+
+    <article>
+      <p class="blog-post-meta"><?= $post['date'] ?> by <?= $post['author'] ?></p>
+      <p><?= $post['content'] ?></p>
+    </article>
+  </main>
 </body>
 
 </html>
--- Assignment 03/index.php
+++ Assignment 03/index.php
@@ -2,7 +2,6 @@
 include_once('functions.php');
 include_once('data.php');
 
-
 ?>
 
 <!DOCTYPE html>
@@ -10,28 +9,42 @@ include_once('data.php');
 
 <head>
   <title>Blog</title>
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="blog.css" rel="stylesheet">
 </head>
 
 <body>
-  <?php
-  /*
-    This is a for loop that displays the titles of all blog posts. Once clicked, the user is sent to the details page with
-    an appropriate query string.
-  */
-  for ($i = 0; $i < count($posts); $i++) {
+  <header class="lh-1 py-3 mb-4">
+    <a class="text-decoration-none" href="index.php">
+      <h1 class="container blog-header-logo">Blog</h1>
+    </a>
+  </header>
+
+  <main class="container">
+    <?php
+    /*
+      This is a for loop that displays the titles of all blog posts. Once clicked, the user is sent to the details page with
+      an appropriate query string.
+    */
+    for ($i = 0; $i < count($posts); $i++) {
+      ?>
+      <div class="col-md-6">
+        <a href='detail.php?index=<?= $i ?>'>
+          <div class="post row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm position-relative">
+            <div class="col p-4 d-flex flex-column position-static">
+              <h3>
+                <?= $posts[$i]['title'] ?>
+              </h3>
+            </div>
+          </div>
+        </a>
+      </div>
+      <?php
+    }
     ?>
-    <div>
-      <a href='detail.php?index=<?= $i ?>'>
-        <h1>
-          <?= $posts[$i]['title'] ?>
-        </h1>
-      </a>
-    </div>
-  <?php
-  }
-  ?>
+  </main>
 </body>
 
 </html>
