961e1950456beb6ee640a7548e4003d76913a983
Owen Newberry <owen@owennewberry.com>
2024-09-17 10:36:57 -0400

    Update detail.php
    Added code to read and display blog contents via the use of a function.

--- Assignment 03/detail.php
+++ Assignment 03/detail.php
@@ -2,16 +2,19 @@
 include_once('functions.php');
 include_once('data.php');
 
-
+$index = $_GET['index'];
+$post = getPost($posts, $index);
 ?>
 
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
   <head>
-    <title></title>
+    <title><?= $post['title'] ?></title>
   </head>
-
   <body>
-
+    <h1><?= $post['title'] ?></h1>
+    <p><strong>Author</strong>:</p> <?= $post['author'] ?></p>
+    <p><strong>Date:</strong></p> <?= $post['date'] ?></p>
+    <p><?= $post['content'] ?></p>
   </body>
 </html>
